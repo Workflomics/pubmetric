d16a753a79c8d88f183452b9d1a6456169a6cb6e
Alma Nilsson <aas.nilsson@gmail.com>
2024-08-13 15:20:52 +0200

    api output format

--- src/pubmetric/api_controller.py
+++ src/pubmetric/api_controller.py
@@ -13,61 +13,77 @@ from pubmetric.network import create_citation_network
 app = FastAPI()
 
 class ScoreResponse(BaseModel):
-    workflow_scores: Dict
+    workflow_scores: List
 
 class GraphRequest(BaseModel):
     topic_id: str
     test_size: str
 
 @app.post("/score_workflows/", response_model=ScoreResponse)
-async def score_workflows(file: UploadFile = File(None)):
-    path_to_data = "out_202407041439" # where does one store things later?  
-    metadata_filename = "../tool_metadata_topic_0121_20240703.json"
+async def score_workflows(cwl_file: UploadFile = File(None)):
+    path_to_data = "out_20240801231111" # where does one store things later?  
 
-    graph = await create_citation_network(inpath=path_to_data) # should I maybe change the name so it does not contain the date, so you have to look in the file instead, or perhaps it is regenerated outside of the package entrirely
-
-    workflow_scores = {}
+    graph = await create_citation_network(inpath=path_to_data, load_graph=True) # should I maybe change the name so it does not contain the date, so you have to look in the file instead, or perhaps it is regenerated outside of the package entrirely
     # Saving the uploaded files temporarily, should this have some type of check so no bad things can be sent? 
-    with tempfile.TemporaryDirectory() as temp_dir: 
+    with tempfile.TemporaryDirectory() as temp_dir:
 
-        file_path = os.path.join(temp_dir, file.filename)
-        with open(file_path, "wb") as f: # Slightly dumb to open and save and then just open again within the parse workflows function. Shoudl I update parse wf to work with open files?
-            f.write(file.file.read())
-        
-        print(os.getcwd())
+        cwl_file_path = os.path.join(temp_dir, cwl_file.filename)
+        with open(cwl_file_path, "wb") as f: # Slightly dumb to open and save and then just open again within the parse workflows function. Shoudl I update parse wf to work with open files?
+            f.write(cwl_file.file.read())
 
-        workflow = parse_cwl_workflows(file_path, metadata_filename)
         
-        workflow_level_scores = {}
+        workflow = parse_cwl_workflows(cwl_filename=cwl_file_path, graph=graph)  
         pmid_workflow = workflow['pmid_edges'] # for the metrics that do not need to take into account the structure
-        workflow_level_scores['workflow_level_average_sum'] = workflow_average_sum(graph, pmid_workflow)
-        workflow_level_scores['connectivity_sum'] = connectivity(graph, workflow)
-        # etc 
 
-        tool_level_scores = {}
-        tool_level_scores['tool_level_average_sum'] = tool_average_sum(graph, workflow)
-        # etc 
+        # Metrics
+        workflow_level_scores =  workflow_average_sum(graph, pmid_workflow)
+        tool_level_scores = tool_average_sum(graph, workflow)
 
-        # Ages
-        with open(metadata_filename, 'r') as f:
-            metadata_file = json.load(f)
+        tool_level_output = []
+        for tool_name, score in tool_level_scores.items():
+            tool_level_output.append({
+                    "desirability": 8.0,# based on the 95th percentile of the nr of cocitations in the proteomics graph
+                    "label": score, # should this be the tool name? or is this what is used on hover? 
+                    "value": score
+                })
 
-        current_year = datetime.now().year
-        tool_ages = {}
-        for step, pmid in workflow['steps'].items(): #TODO: maybe store age directly, instead of publication date - i use age often. pub year never. then we can also use this list comp nightmare below
-            tool_name = step.split("_")[0]
-            tool_ages[tool_name] = next(tool['age'] for tool in graph.vs if tool['pmid'] == pmid)
+        ages_output = []
+        ages = []
+        for _, pmid in workflow['steps'].items():
+            age = next(tool['age'] for tool in graph.vs if tool['pmid'] == pmid)
+            ages.append(age)
+            ages_output.append({
+                    "desirability": 0, # Because green == new?  
+                    "label": age, # should this be the tool name? or is this what is used on hover? 
+                    "value": age
+                })
 
-        # Format returned scores
-        scores = {  'workflow_level_scores': workflow_level_scores,
-                    'tool_level_scores': tool_level_scores,
-                    'ages': tool_ages                    
-                    }
+
         
-        workflow_id = file.filename  # filename as the workflow id
-        workflow_scores[workflow_id] = scores # do I want to save any other info? # do I need the id there? 
-    
-    return ScoreResponse(workflow_scores=workflow_scores)
+        metric_benchmark = {
+            "unit": "metric",
+            "description": "The tool- and workflow-level metric",
+            "title": "Pubmetric",
+            "steps": tool_level_output,
+            "aggregate_value": {
+                "desirability": 8.0, # again based on the 95th percentile 
+                "value": workflow_level_scores 
+            }
+        }
+        age_benchmark = {
+            "unit": "age",
+            "description": "Ages of primary publications",
+            "title": "Age",
+            "steps": ages_output,
+            "aggregate_value": {
+                "desirability": 1, # unclear what to put here
+                "value": np.median(ages) # median age as the aggregate value? 
+            }
+        }
+
+    benchmarks = [metric_benchmark, age_benchmark]   
+
+    return ScoreResponse(workflow_scores=benchmarks)
 
 @app.post("/recreate_graph/")
 async def recreate_graph(graph_request: GraphRequest):
