ac6f240ad9ac1945781655f8ecf17282a799ce6d
Alma Nilsson <aas.nilsson@gmail.com>
2024-08-13 16:47:24 +0200

    Documentation

--- src/pubmetric/api_controller.py
+++ src/pubmetric/api_controller.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, UploadFile, File
 from pydantic import BaseModel
-from typing import Dict
+from typing import Dict, Optional, List
 import tempfile
 import os 
 from contextlib import asynccontextmanager
@@ -17,7 +17,7 @@ jobstores = {
     'default': MemoryJobStore()
 }
 
-scheduler = AsyncIOScheduler(jobstores=jobstores, timezone='Asia/Kolkata')
+scheduler = AsyncIOScheduler(jobstores=jobstores, timezone='Europe/Berlin')
 
 latest_output_path = "out_20240801231111"
 
@@ -38,9 +38,17 @@ class ScoreResponse(BaseModel):
 class GraphRequest(BaseModel):
     topic_id: str
     test_size: str
+    tool_list: Optional[list]
 
 @scheduler.scheduled_job('interval', days=30)
 async def periodic_graph_generation():
+    """Periodically generates a new citation network graph every 30 days and updates the global path if the graph is successfully created. 
+        If the graph file is not found, it logs an error message.
+
+    :return: Dict
+        A dictionary containing a message with the new graph path if successful or an error message if the graph file is not found.
+
+    """
     global latest_output_path
     try:
         new_output_path = await create_citation_network(topic_id="default", test_size=20, return_path = True)
@@ -55,6 +63,15 @@ scheduler.add_job(periodic_graph_generation, 'interval', days=30)
 
 @app.post("/score_workflows/", response_model=ScoreResponse)
 async def score_workflows(cwl_file: UploadFile = File(None)):
+    """
+    Processes an uploaded CWL file to score workflows based on the current citation network graph. Returns scores for the tool- and workflow-level metrics, and ages.
+
+    :param cwl_file: UploadFile
+        The uploaded CWL file to be processed.
+
+    :return: ScoreResponse
+        A response model containing the computed scores for the workflow; metric and age benchmarks accoring to the Workflomics JSON Schema for Benchmarks.
+    """
 
     graph = await create_citation_network(inpath=latest_output_path, load_graph=True) # should I maybe change the name so it does not contain the date, so you have to look in the file instead, or perhaps it is regenerated outside of the package entrirely
     # Saving the uploaded files temporarily, should this have some type of check so no bad things can be sent? 
@@ -90,9 +107,7 @@ async def score_workflows(cwl_file: UploadFile = File(None)):
                     "label": age, # should this be the tool name? or is this what is used on hover? 
                     "value": age
                 })
-
-
-        
+   
         metric_benchmark = {
             "unit": "metric",
             "description": "The tool- and workflow-level metric",
@@ -120,6 +135,18 @@ async def score_workflows(cwl_file: UploadFile = File(None)):
 
 @app.post("/recreate_graph/")
 async def recreate_graph(graph_request: GraphRequest): # if you want to recreate it with a request
+    """
+    Recreates the citation network graph based on the provided topic ID and test size. 
+    Updates the global graph path if the graph is successfully generated.
+
+    :param graph_request: GraphRequest
+        The request model containing the topic ID and test size for graph recreation.
+
+    :return: Dict
+        A dictionary containing a message with the new graph path if successful or an error message if the graph file is not found.
+
+
+    """
     global latest_output_path
     try:
         new_output_path = await create_citation_network(topic_id=graph_request.topic_id, test_size=20, return_path=True)
